7e0c4b155b2ddf6629ff6a405c722d3468cc4b10
abdulmoktadir <abdulmoktadir2010@gmail.com>
2026-02-14 23:12:34 +0800

    Update IVFFS_Delphi_WINGS_TODIM.py

--- IVFFS_Delphi_WINGS_TODIM.py
+++ IVFFS_Delphi_WINGS_TODIM.py
@@ -87,7 +87,7 @@ def ivffs_score(v):
 # WINGS LINGUISTIC SCALES
 # =========================================================
 
-Strength evaluation
+#Strength evaluation
 WINGS_STRENGTH = {
     "VLI": make_ivffs(0.10, 0.20, 0.80, 0.90),
     "LI":  make_ivffs(0.20, 0.50, 0.70, 0.80),
@@ -102,7 +102,7 @@ WINGS_STRENGTH_FULL = {
     "HI":  "High Important",
     "VHI": "Very High Important",
 }
-Influence evaluation
+#Influence evaluation
 WINGS_INFLUENCE = {
     "ELI": make_ivffs(0.05, 0.15, 0.85, 0.95),
     "VLI": make_ivffs(0.15, 0.25, 0.75, 0.85),
